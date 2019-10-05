5fe542e3f28823a375f6c78019dc410c8beece93
Matt C <matthew.critelli@gmail.com>
2019-10-05 13:59:33 -0600

    Refactor find method to simplify.

--- js/data_structures/trees/bst/bst.js
+++ js/data_structures/trees/bst/bst.js
@@ -14,95 +14,112 @@ class BinarySearchTree {
   }
 
   traverse(node, value) {
-    if(node.value === value) return undefined;
+    return value < node.value
+      ? node.left
+      : node.right
+  }
 
-    if(value < node.value) {
-      return node.left;
+  find(value) {
+    if(!this.root) return false;
+
+    let currNode = this.root;
+
+    while (currNode !== null) {
+      if (currNode.value === value) return true;
+      currNode = this.traverse(currNode, value);
     }
-    return node.right;
+    return false;
   }
 
   insert(value) {
     const newNode = new Node(value);
 
-    if(!this.root) {
-      this.root = newNode;
-    } else {
-      let currNode = this.root;
-      let nextNode = this.traverse(currNode, value);
-      
-      while (nextNode !== null) {
-        if (nextNode === undefined) return undefined;
-        
-        currNode = nextNode;
-        nextNode = this.traverse(currNode, value);
-      }
+    if(!this.root) this.root = newNode;
+    if(this.root.value === value) return undefined;
 
-      if (value < currNode.value) {
-        currNode.left = newNode;
-      } else {
-        currNode.right = newNode;
-      }
+    let currNode = this.root;
+    let nextNode = this.traverse(currNode, value);
+    
+    while (nextNode !== null) {
+      if (nextNode.value === value) return undefined;
+      
+      currNode = nextNode;
+      nextNode = this.traverse(currNode, value);
     }
+
+    if (value < currNode.value) {
+      currNode.left = newNode;
+    } else {
+      currNode.right = newNode;
+    }
+
     return this;
   }
-
-  // insert(value) {
-  //   const newNode = new Node(value);
-
-  //   if(!this.root) {
-  //     this.root = newNode;
-  //   } else {
-  //     let currNode = this.root;
-  //     let found = false;
-
-  //     while(!found) {
-  //       if (value < currNode.value) {
-  //         if(currNode.left) {
-  //           currNode = currNode.left;
-  //         } else {
-  //           currNode.left = newNode;
-  //           found = true;
-  //         }
-  //       } else if(value > currNode.value) {
-  //         if(currNode.right) {
-  //           currNode = currNode.right;
-  //         } else {
-  //           currNode.right = newNode;
-  //           found = true;
-  //         }
-  //       }
-  //     }
-  //   }
-  //   return this;
-  // }
-
-
 }
 
 let bst = new BinarySearchTree();
 
-console.log('\n\nempty bst\n', bst);
+// console.log('\n\nempty bst\n', bst);
 
 bst.insert(50);
 
-console.log('\n\nbst with root value of 50\n', bst);
+// console.log('\n\nbst with root value of 50\n', bst);
 
 bst.insert(30);
 
-console.log('\n\nbst insert 30\n', bst);
+// console.log('\n\nbst insert 30\n', bst);
 
 bst.insert(80);
 
-console.log('\n\nbst insert 80\n', bst);
+// console.log('\n\nbst insert 80\n', bst);
 
 bst.insert(45);
 bst.insert(35);
-bst.insert(85);
+bst.insert(40);
 bst.insert(90);
 bst.insert(95);
 bst.insert(100);
 bst.insert(87);
 
 // console.log('\n\nbst insert 4\n', bst);
-console.log('\n\nbst at end\n', util.inspect(bst, { showHidden: false, depth: null }))
+// console.log('\n\nbst at end\n', util.inspect(bst, { showHidden: false, depth: null }))
+
+console.log(bst.find(40));
+
+
+
+
+
+
+
+// OLD SOLUTIONS //
+
+// insert(value) {
+//   const newNode = new Node(value);
+
+//   if(!this.root) {
+//     this.root = newNode;
+//   } else {
+//     let currNode = this.root;
+//     let found = false;
+
+//     while(!found) {
+//       if (value < currNode.value) {
+//         if(currNode.left) {
+//           currNode = currNode.left;
+//         } else {
+//           currNode.left = newNode;
+//           found = true;
+//         }
+//       } else if(value > currNode.value) {
+//         if(currNode.right) {
+//           currNode = currNode.right;
+//         } else {
+//           currNode.right = newNode;
+//           found = true;
+//         }
+//       }
+//     }
+//   }
+//   return this;
+// }
